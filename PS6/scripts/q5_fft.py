c8ae7cd1ddfed8e1c8df34843d4df2219d995b38
sheilsarda <sheilsarda@yahoo.com>
2021-04-12 13:43:06 -0400

    fixed fft script with comments

--- PS6/scripts/q5_fft.py
+++ PS6/scripts/q5_fft.py
@@ -25,7 +25,6 @@ def sine_wave(f,overSampRate,phase,nCyl):
 	g = np.sin(2*np.pi*f*t+phase) # replace with cos if a cosine wave is desired
 	return (t,g) # return time base and signal g(t) as tuple
 
-
 def box_wave(f,overSampRate,phase,nCyl):
 	"""
 	Generate sine wave signal with the following parameters
@@ -67,16 +66,25 @@ def cos_wave(f,overSampRate,phase,nCyl):
 	return (t,g) # return time base and signal g(t) as tuple
 
 
+"""
+Parameters for function to model
+"""
 nCyl = 1.5 # desired number of cycles of the sine wave
 f = nCyl/128 #frequency = 10 Hz
 overSampRate = 20 #oversammpling rate
 fs = f*overSampRate #sampling frequency
 phase = np.pi * 0.65 # 1/3*np.pi #phase shift in radians
 
-
+"""
+Select function generator
+"""
+# (t,x) = cos_wave(f,overSampRate,phase,nCyl) #function call
 # (t,x) = sine_wave(f,overSampRate,phase,nCyl) #function call
 (t,x) = box_wave(f,overSampRate,phase,nCyl) #function call
 
+"""
+Uncomment this section to plot the raw function
+"""
 # plt.plot(t,x) # plot using pyplot library from matplotlib package
 # plt.title('Sine wave f='+str(f)+' Hz') # plot title
 # plt.xlabel('Time (s)') # x-axis label
@@ -84,32 +92,14 @@ phase = np.pi * 0.65 # 1/3*np.pi #phase shift in radians
 # plt.show() # display the figure
 # exit()
 
-# NFFT=1024 #NFFT-point DFT  
-# X=fft(x,NFFT) #compute DFT using FFT     
-# fig2, ax = plt.subplots(nrows=1, ncols=1) #create figure handle
-# nVals=np.arange(start = 0,stop = NFFT)/NFFT #Normalized DFT Sample points         
-# ax.plot(nVals,np.abs(X))     
-# ax.set_title('Double Sided FFT - without FFTShift')        
-# ax.set_xlabel('Normalized Frequency')
-# ax.set_ylabel('DFT Values')
-# fig2.show()
-# plt.show() # display the figure
 
-
+"""
+This section does the FFT and FFTshift
+"""
 NFFT=1024     
 X=fftshift(fft(x,NFFT))
-
 fig4, ax = plt.subplots(nrows=1, ncols=1) #create figure handle
-
 fVals=np.arange(start = -NFFT/2,stop = NFFT/2)*fs/NFFT
-
-# series = np.abs(X)
-# occ_1 = np.argmax(series)
-# occ_2 = np.argmax(series[occ_1:])
-# res = fVals[occ_2] - fVals[occ_1]
-# print("Gap between peaks: " + str(res))
-# breakpoint()
-
 ax.plot(fVals,np.abs(X),'b')
 ax.set_title('Double Sided FFT - with FFTShift')
 ax.set_xlabel('Frequency (Hz)')         
